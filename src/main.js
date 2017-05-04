741666d98d36fe0656118462ae7075c3ef94ba96
Juan-Carlos Pacheco <juapache2@cobogpgp8228.global.publicisgroupe.net>
2017-05-04 16:18:55 -0500

    commit files

---
+++ src/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	//loading the base html
+	 	$("#page").load("components/page/page.html");
+  		$("header").load("components/header/header.html");
+        $("#breadcrumb").load("components/breadcrumbs/breadcrumbs.html");
+        $("footer").load("components/footer/footer.html");
+        $(".leftColumn").load("components/shop/shop.html");
+        $(".rightColumn").load("components/shoppingcart/shoppingcart.html");
+
+        
+});
